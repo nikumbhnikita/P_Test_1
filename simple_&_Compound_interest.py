e7dbd18f1c52339ce56d3fde0103a54d38afd720
nikumbhnikita <117893416+nikumbhnikita@users.noreply.github.com>
2022-11-19 20:46:23 +0530

    Add files via upload

---
+++ simple_&_Compound_interest.py
@@ -0,0 +1,16 @@
+#Q77. Write a Python program to calculate the simple interest and Compound interest. Formula to calculate simple interest is 
+# SI = (PRT)/100
+principal = 1000
+Rate = 10
+Time =2
+SI= (principal*Rate*Time)/100
+A = principal*(1+ Rate/100)**Time
+print("Simple interest is :",SI)
+print("Compound interest is :",A)
+
+
+#Q78. Write a Python program to calculate the compound interest. Formula of compound interest is 
+# A = P(1+ R/100^t.
+
+
+
